3951c2d90b615459f2464fdf25830f2c41c95c63
chcknlegwill <WillN70@outlook.com>
2022-09-25 22:56:25 +0100

    Fixed .gitignore & modified server.js + 404.html

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.get("/", (req, res) => {
     res.status(200).sendFile("./server/public/html/index.html", { root: __dirname})
 });
 
-/*
+
 app.get("/*", (req, res) => {
     res.status(404).sendFile("./server/public/html/404.html", { root: __dirname })
 }); 
-*/
+
 // starting api usage here - I will need to server the main page for this as the 404 page is not made for it.
 // note to self -> add middleware
 
