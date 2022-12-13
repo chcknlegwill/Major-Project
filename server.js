cbbe48b6d3f69e8fe3ee65c55092226fc5b0f5f4
chcknlegwill <112222403+chcknlegwill@users.noreply.github.com>
2022-12-13 10:47:24 +0000

    Added logger from NPM called winston, not sure if its the right choice - it still needs a lot of work. modified all .json files & server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { th } = require("date-fns/locale");
 const express = require("express");
 const app = express();
 const path = require("path");
+const logger = require("/")
+
 //This is to import npm (Node Package Manager) Modules such as date-fns to  
 
 //import middleware below
@@ -12,21 +14,29 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "/")));
 
-
-app.get("/login", (req, res) => {
-	res.status(200).sendFile("./server/public/html/login.html", { root: __dirname })
-}); //serves users login page 
-
-
-
-
 app.get("/*", (req, res) => {
 	res.status(404).sendFile("./server/public/html/404.html", { root: __dirname })
 
-});  // This picks up every request unless it is also served e.g. "/"
+});
+
+app.get("/login", (req, res) => {
+	res.status(200).sendFile("./server/public/html/login.html")
+});
+console.log(__dirname);
+app.listen(PORT, () =>  {
+	console.log(`Server running on port: ${PORT}`);
+	
+});
+
+
+/*
+app.get("/login", (req, res) => {	res.status(200).sendFile("./server/public/html/login.html", { root: __dirname })
+}); //serves users login page
+*/
+
+
+
+  // This picks up every request unless it is also served e.g. "/"
 
 
 //  __dirname =   /home/chcknlegwill/Major-Project/server/public/html
-
-console.log(__dirname);
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
--- server/middlware/logger.js
+++ server/middlware/logger.js
@@ -1,6 +1,35 @@
 
 //Re-watch DG video on this in order to effectivley use this.
 //This cleans up the code on server.js when handling different requests - still needs a lot of work 
-const logger = (req, res, next) => {
-    
-}
+
+//using this npm module called "winston" for logging
+
+/* // below is default code from the winston docs
+app.use(expressWinston.logger({
+	transports: [
+		new winston.transports.Console()
+	],
+	format: winston.format.combine(
+		winston.format.colorize(),
+		winston.format.json()
+	),
+	meta: true,
+	msg: "HTTP {{req.method}} {{req.url}}",
+	expressFormat: true,
+	colorize: true,
+	ignoreRoute: function(req, res) { return false; }
+}));
+
+app.use(router);
+
+*/
+const winston = require("winston");
+expressWinston = require("express-winston");
+
+const logger = winston.createLogger({
+	level: "debug",
+	format: winston.fomat.json(),
+	transports: [new winston.transports.Console()],
+});
+
+module.exports = logger;
